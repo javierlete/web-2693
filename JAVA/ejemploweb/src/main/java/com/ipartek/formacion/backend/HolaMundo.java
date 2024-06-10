1e288a2606a0d169367f1a549a69425fb861d31b
Javier Lete <javier@eskura21.com>
2024-06-10 11:04:19 +0200

    Servlet de ejemplo

---
+++ JAVA/ejemploweb/src/main/java/com/ipartek/formacion/backend/HolaMundo.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hola")
+public class HolaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.printf("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+				<h1>%s</h1>
+</body>
+</html>
+				""", "pepe");
+	}
+
+}
