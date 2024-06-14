8c08bbd89a6e09fc5200f2a6776b4c4a1e261f4e
Javier Lete <javier@eskura21.com>
2024-06-14 10:33:59 +0200

    Ejemplo de JDBC

---
+++ JAVA/basico/src/basico/EjemploJDBC.java
@@ -0,0 +1,22 @@
+package basico;
+
+import java.sql.*;
+
+public class EjemploJDBC {
+	private static final String SQL_SELECT = "SELECT * FROM productos";
+
+	public static void main(String[] args) throws SQLException {
+//		String URL = "jdbc:mysql://localhost:3306/basededatos";
+		final String URL = "jdbc:sqlite:C:\\Users\\html.IPARTEKAULA\\WEB\\JAVA\\ejemploweb\\sql\\ejemploweb.db";
+		final String USER = "usuario"; // En SQLite se ignora
+		final String PASS = "contraseña"; // En SQLite se ignora
+		
+		Connection con = DriverManager.getConnection(URL, USER, PASS);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(SQL_SELECT);
+		
+		while(rs.next()) {
+			System.out.printf("%-10s %-20s %10s\n", rs.getString("id"), rs.getString("nombre"), rs.getString("precio"));
+		}
+	}
+}
