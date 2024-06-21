96e00f2d1f50500147242f3a84c1ae93f9c8bc10
Javier Lete <javier@eskura21.com>
2024-06-21 12:41:36 +0200

    Cambiamos IndexServlet para usar la biblioteca compartida

--- JAVA/biblioteca/src/main/java/com/ipartek/formacion/biblioteca/controladores/IndexServlet.java
+++ JAVA/biblioteca/src/main/java/com/ipartek/formacion/biblioteca/controladores/IndexServlet.java
@@ -1,9 +1,8 @@
 package com.ipartek.formacion.biblioteca.controladores;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import com.ipartek.formacion.biblioteca.modelos.Libro;
+import com.ipartek.formacion.biblioteca.accesodatos.Biblioteca;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,14 +15,8 @@ public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		var libros = new ArrayList<Libro>();
 		
-		libros.add(new Libro(1L, "Java en primero", "El mejor", false, "java-primero"));
-		libros.add(new Libro(2L, "C# en primero", "Muy bueno", true, "csharp-primero"));
-		libros.add(new Libro(3L, "Servlets en segundo", "Interesante", true, "servlets-segundo"));
-		libros.add(new Libro(4L, "JavaScript en un año", "Buff", false, "javascript-anno"));
-
-		request.setAttribute("libros", libros);
+		request.setAttribute("libros", Biblioteca.libros);
 		
 		request.getRequestDispatcher("libros.jsp").forward(request, response);
 	}
